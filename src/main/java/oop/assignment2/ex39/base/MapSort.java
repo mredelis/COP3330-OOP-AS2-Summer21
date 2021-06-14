c46ed326f76870ac946f624abcdc68186b95dab3
Edelis Molina <edelitamr@gmail.com>
2021-06-13 21:16:26 -0400

    Exercise 39 and 40. Some clean out

---
+++ src/main/java/oop/assignment2/ex39/base/MapSort.java
@@ -0,0 +1,60 @@
+package oop.assignment2.ex39.base;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapSort {
+
+    public List<Map<String, String>> createListOfEmployeeMapsToSort(){
+
+        List<Map<String, String>> employeeList = new ArrayList<>();
+
+        // map1 employee 1
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("first", "John");
+        map1.put("last", "Johnson");
+        map1.put("position", "Manager");
+        map1.put("date", "2016-12-31");
+        // map2 employee 2
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("first", "Tou");
+        map2.put("last", "Xian");
+        map2.put("position", "Software Engineer");
+        map2.put("date", "2016-10-05");
+        // map3 employee 3
+        Map<String, String> map3 = new HashMap<>();
+        map3.put("first", "Michaela");
+        map3.put("last", "Michelson");
+        map3.put("position", "District Manager");
+        map3.put("date", "2015-12-19");
+        // map4 employee 4
+        Map<String, String> map4 = new HashMap<>();
+        map4.put("first", "Jake");
+        map4.put("last", "Jacobson");
+        map4.put("position", "Programmer");
+        map4.put("date", "");
+        // map5 employee 5
+        Map<String, String> map5 = new HashMap<>();
+        map5.put("first", "Jacquelyn");
+        map5.put("last", "Jackson");
+        map5.put("position", "DBA");
+        map5.put("date", "");
+        // map6 employee 6
+        Map<String, String> map6 = new HashMap<>();
+        map6.put("first", "Sally");
+        map6.put("last", "Webber");
+        map6.put("position", "Web Developer");
+        map6.put("date", "2015-12-18");
+
+        employeeList.add(map1);
+        employeeList.add(map2);
+        employeeList.add(map3);
+        employeeList.add(map4);
+        employeeList.add(map5);
+        employeeList.add(map6);
+
+        return employeeList;
+    }
+}
--- src/main/java/oop/assignment2/ex39/base/SortRecordsApplication.java
+++ src/main/java/oop/assignment2/ex39/base/SortRecordsApplication.java
@@ -5,92 +5,35 @@ import java.util.*;
 public class SortRecordsApplication {
     public static void main(String[] args) {
 
-        // List of maps. myList is employee list
-        List<Map<String, String>> myList = new ArrayList<>();
+        SortRecordsApplication app = new SortRecordsApplication();
 
-        // map1 employee 1
-        Map<String, String> map1 = new HashMap<>();
-        map1.put("first", "John");
-        map1.put("last", "Johnson");
-        map1.put("position", "Manager");
-        map1.put("date", "2016-12-31");
-        // map2 employee 2
-        Map<String, String> map2 = new HashMap<>();
-        map2.put("first", "Tou");
-        map2.put("last", "Xian");
-        map2.put("position", "Software Engineer");
-        map2.put("date", "2016-10-05");
-        // map3 employee 3
-        Map<String, String> map3 = new HashMap<>();
-        map3.put("first", "Michaela");
-        map3.put("last", "Michelson");
-        map3.put("position", "District Manager");
-        map3.put("date", "2015-12-19");
-        // map4 employee 4
-        Map<String, String> map4 = new HashMap<>();
-        map4.put("first", "Jake");
-        map4.put("last", "Jacobson");
-        map4.put("position", "Programmer");
-        map4.put("date", "");
-        // map5 employee 5
-        Map<String, String> map5 = new HashMap<>();
-        map5.put("first", "Jacquelyn");
-        map5.put("last", "Jackson");
-        map5.put("position", "DBA");
-        map5.put("date", "");
-        // map6 employee 6
-        Map<String, String> map6 = new HashMap<>();
-        map6.put("first", "Sally");
-        map6.put("last", "Webber");
-        map6.put("position", "Web Developer");
-        map6.put("date", "2015-12-18");
+        // List of maps.
+        MapSort listOfMaps = new MapSort();
+        List<Map<String, String>> employeeList = listOfMaps.createListOfEmployeeMapsToSort();
 
-        myList.add(map1);
-        myList.add(map2);
-        myList.add(map3);
-        myList.add(map4);
-        myList.add(map5);
-        myList.add(map6);
+        app.printColumnHeadings("Here is the unsorted list");
+        app.printTable(employeeList);
 
-        System.out.println("\nHere is the unsorted list!\n");
-        System.out.printf("%-30s%s%-25s%s%-20s%n", "Name","| ", "Position", "| ", "Separation Date"); // column headings
+        Collections.sort(employeeList, (m1, m2) -> m1.get("last").compareTo(m2.get("last")));
+
+        app.printColumnHeadings("Here is the sorted list!");
+        app.printTable(employeeList);
+
+    }
+
+
+    public void printColumnHeadings(String prompt){
+        System.out.println("\n" + prompt + "\n");
+        System.out.printf("%-30s%s%-25s%s%-20s%n", "Name","| ", "Position", "| ", "Separation Date");
         System.out.printf("%-30s%s%-25s%s%-20s%n", "------------------------------", "|",
-                                                   "-------------------------", " |", "--------------------");
-        for (Map<String, String> stringStringMap : myList) {
-            System.out.printf("%-30s%s%-25s%s%-20s%n", stringStringMap.get("first") + " " + stringStringMap.get("last"),
-                              "|", stringStringMap.get("position"), " |", stringStringMap.get("date"));
-        }
+                          "-------------------------", " |", "--------------------");
+    }
 
 
-
-//        System.out.println(myList.get(0).keySet());
-        Collections.sort(myList, (m1, m2) -> m1.get("last").compareTo(m2.get("last")));
-        System.out.println("\nHere is the sorted list!\n");
-
-        System.out.printf("%-30s%s%-25s%s%-20s%n", "Name","| ", "Position", "| ", "Separation Date"); // column headings
-        System.out.printf("%-30s%s%-25s%s%-20s%n", "------------------------------", "|",
-            "-------------------------", " |", "--------------------");
-        for (Map<String, String> stringStringMap : myList) {
+    public void printTable(List<Map<String, String>> eList){
+        for (Map<String, String> stringStringMap : eList) {
             System.out.printf("%-30s%s%-25s%s%-20s%n", stringStringMap.get("first") + " " + stringStringMap.get("last"),
                 "|", stringStringMap.get("position"), " |", stringStringMap.get("date"));
         }
-
-
-
-
-
-
-//        System.out.println(myList.get(0).get("first"));
-//
-//        System.out.println(myList.get(0).get("last"));
-//        System.out.println(myList.get(0).get("position"));
-//        System.out.println(myList.get(0).get("date"));
-
-//        myList.forEach( (n) -> { System.out.println(n); } );
-
-
-
-
-
     }
 }
